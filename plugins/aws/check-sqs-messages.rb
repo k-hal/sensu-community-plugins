9fb90ac91d81002a964ded07d27bc6dda605b0e3
HAL <koichi.h+github@gmail.com>
2015-02-20 15:31:37 +0900

    fix region parameter

--- plugins/aws/check-sqs-messages.rb
+++ plugins/aws/check-sqs-messages.rb
@@ -89,6 +89,7 @@ class SQSMsgs < Sensu::Plugin::Check::CLI
     hash.update access_key_id: config[:aws_access_key], secret_access_key: config[:aws_secret_access_key]\
       if config[:aws_access_key] && config[:aws_secret_access_key]
     hash
+    hash.update region: config[:aws_region]
   end
 
   def run
